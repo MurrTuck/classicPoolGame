e063e80e35987fed348b21432e698bdb421d58da
Murray Tucker <murraytucker@Murrays-MacBook-Pro.local>
2018-06-07 18:36:05 -0600

    2nd test commit

--- Assets.js
+++ Assets.js
@@ -1,1 +1,20 @@
 let sprites = {};
+let assetsStillLoading = 0;
+
+function loadAssets(callback){
+
+    function loadSprite(fileName){
+        assetsStillLoading++;
+
+        let spriteImage = new Image();
+        spriteImage.src = "./assets/sprites/" + fileName;
+
+        spriteImage.onload = function(){
+            assetsStillLoading--;
+        }
+
+        return spriteImage;
+    }
+    sprites.background = loadSprite('spr_background4.png')
+    sprites.stick = loadSprite('spr_stick.png')
+}
