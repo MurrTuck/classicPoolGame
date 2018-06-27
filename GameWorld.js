605db0c90e7a949dc36088816ebf691f4d639246
MurrTuck <murrtuck@gmail.com>
2018-06-27 01:11:17 -0600

    Starting the colision effect

--- GameWorld.js
+++ GameWorld.js
@@ -1,31 +1,63 @@
-const DELTA = 1/100;
+const DELTA = 1 / 100;
 
-function GameWorld(){
+function GameWorld() {
+  this.balls = [
+    [new Vector2(1022, 413), COLOR.YELLOW], //1
+    [new Vector2(1056, 393), COLOR.YELLOW], //2
+    [new Vector2(1056, 433), COLOR.RED], //3
+    [new Vector2(1090, 374), COLOR.RED], //4
+    [new Vector2(1090, 413), COLOR.BLACK], //5
+    [new Vector2(1090, 452), COLOR.YELLOW], //6
+    [new Vector2(1126, 354), COLOR.YELLOW], //7
+    [new Vector2(1126, 393), COLOR.RED], //8
+    [new Vector2(1126, 433), COLOR.YELLOW], //9
+    [new Vector2(1126, 472), COLOR.RED], //10
+    [new Vector2(1162, 335), COLOR.RED], //11
+    [new Vector2(1162, 374), COLOR.RED], //12
+    [new Vector2(1162, 413), COLOR.YELLOW], //13
+    [new Vector2(1162, 452), COLOR.RED], //14
+    [new Vector2(1162, 491), COLOR.YELLOW], //15
+    [new Vector2(413, 413), COLOR.WHITE]
+  ].map(params => new Ball(params[0], params[1]));
 
-    this.whiteBall = new Ball(new Vector2(413, 413));
-    this.stick = new Stick(new Vector2(413, 413),this.whiteBall.shoot.bind(this.whiteBall)
-    );
+  this.whiteBall = this.balls[this.balls.length - 1]
+  this.stick = new Stick(
+    new Vector2(413, 413),
+    this.whiteBall.shoot.bind(this.whiteBall)
+  );
 }
 
-GameWorld.prototype.update = function(){
+GameWorld.prototype.update = function() {
+  this.stick.update();
+  
+  for( let i = 0; i < this.balls.length; i++){
+      this.balls[i].update(DELTA);
+  }
 
-    this.stick.update();
-    this.whiteBall.update(DELTA);
+  if (!this.ballsMoving() && this.stick.shot) {
+    this.stick.reposition(this.whiteBall.position);
+  }
+};
 
-    if(!this.ballsMoving() && this.stick.shot){
-        this.stick.reposition(this.whiteBall.position);
+GameWorld.prototype.draw = function() {
+  Canvas.drawImage(sprites.background, {x:0, y:0});
+
+    for (let i = 0; i < this.balls.length; i++) {
+        this.balls[i].draw(DELTA);
     }
 
-}
+  this.stick.draw();
+};
 
-GameWorld.prototype.draw = function (){
+GameWorld.prototype.ballsMoving = function() {
+  
+    let ballsMoving = false;
 
-    Canvas.drawImage(sprites.background, {x:0, y:0});
-
-        this.stick.draw()
-        this.whiteBall.draw()
-}
-
-GameWorld.prototype.ballsMoving = function(){
-    return this.whiteBall.moving;
-}
+    for( let i = 0 ; i < this.balls.length ; i++){
+        if(this.balls[i].moving){
+            ballsMoving = true;
+            break;
+        }
+    }
+    return ballsMoving;
+};
